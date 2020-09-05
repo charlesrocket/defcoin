f462f15d7dcd6e9c30cfe07a29ce0f010ea98757
-k <slowdive@me.com>
2020-09-04 17:43:04 -0700

    TS fixes

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -114,9 +114,9 @@ enum WalletFeature
     FEATURE_WALLETCRYPT = 40000, // wallet encryption
     FEATURE_COMPRPUBKEY = 60000, // compressed public keys
 
-    FEATURE_HD = 130000, // Hierarchical key derivation after BIP32 (HD Wallet)
+    FEATURE_HD = 90000, // Hierarchical key derivation after BIP32 (HD Wallet)
 
-    FEATURE_HD_SPLIT = 139900, // Wallet with HD chain split (change outputs will use m/0'/1'/k)
+    FEATURE_HD_SPLIT = 90000, // Wallet with HD chain split (change outputs will use m/0'/1'/k)
 
     FEATURE_NO_DEFAULT_KEY = 159900, // Wallet without a default key written
 
