4daa401aa38b07462226caa539eaadd2f339d0cf
-k <slowdive@me.com>
2022-08-10 18:10:24 -0700

    Fix ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -44,7 +44,7 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN) {
-        return MakeUnique<CBaseChainParams>("", 1335, 1336);
+        return MakeUnique<CBaseChainParams>("", 1336, 1337);
     } else if (chain == CBaseChainParams::TESTNET) {
         return MakeUnique<CBaseChainParams>("testnet4", 11335, 11336);
     } else if (chain == CBaseChainParams::SIGNET) {
